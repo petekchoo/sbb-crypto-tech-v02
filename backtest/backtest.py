754a3793c5bca14d2b50540584b7e31e9a99cef7
Peter Choo <peterchoo@Peters-MBP.lan>
2022-02-19 12:50:09 -0500

    backtest update

--- backtest/backtest.py
+++ backtest/backtest.py
@@ -1,3 +1,6 @@
 # backtest.py: handles any backtest logic, including train-test split, scenario analysis, 
 #                      visualizations, and anything like that
 #         # any other testing files / results / outputs
+
+def backTest(data, strategy):
+    return "backtest successful!"
