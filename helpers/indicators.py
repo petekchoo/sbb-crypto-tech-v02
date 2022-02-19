78681ec7460dcc0546b04c3fe0cd5b964bfa320b
Peter Choo <peterchoo@Peters-MBP.lan>
2022-02-19 17:51:27 -0500

    rising function v0.1

--- helpers/indicators.py
+++ helpers/indicators.py
@@ -5,3 +5,47 @@
 #             # golden_cross(data, short_window, long_window) --> signal
 #             # death_cross(data, short_window, long_window) --> signal
 #             # ... any other TA signals
+
+import csv
+import datetime
+
+lstDict = []
+reader = csv.DictReader(open('data/test-rise.csv', mode='r', encoding='utf-8-sig'))
+lstDict = list(reversed(list(reader)))
+
+# Basic directional: objective rising, falling based on impulsive moves and pullbacks
+
+def risingCheck(data):
+
+    currentLow = float(data[0]["low"])
+    pullbackLow = float(data[0]["low"])
+    
+    highestHigh = float(data[0]["high"])
+    boolRising = True
+
+    for candle in data:
+        
+        # If low is lower than pullbackLow, return False
+        if float(candle["low"]) < pullbackLow:
+            boolRising = False
+
+        # If low is lower than lowestCurrent and higher than pullbackLow
+        #   set currentLow to low
+        elif float(candle["low"]) < currentLow and float(candle["low"]) > pullbackLow:
+            currentLow = float(candle["low"])
+        
+        # If high is new highest, update highestHigh and set pullbackLow to currentLow
+        elif float(candle["high"]) > highestHigh:
+            highestHigh = float(candle["high"])
+            pullbackLow = currentLow
+            currentLow = float(candle["low"])
+
+    return boolRising
+
+print(risingCheck(lstDict))
+
+# Key indicators: ATR, moving averages (20, 50, 200), RSI
+
+# Candlestick patterns: 32.8%, engulfing, 
+
+# Chart patterns: double tops, double bottoms
--- helpers/strategy.py
+++ helpers/strategy.py
@@ -1,3 +1,16 @@
 # strategy.py: takes in parameters for a trading day (i.e. account, current date signals)
 #                     and executes trade decision
 #             # trading_day() --> 
+
+# Read relevant csv files into dictionaries
+
+import csv
+import datetime
+
+dictSample = csv.DictReader(open('data/sample.csv', mode='r', encoding='utf-8-sig'))
+# dict200 = 
+
+for item in dictSample:
+    print(datetime.datetime.fromtimestamp(int(item["time"])))
+    print("high:", item["high"])
+    print("low:", item["low"])
